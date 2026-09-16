d41d7d15c09d5e1416c93f0969063149ac8c21c0
Aleksandr202020 <aleksandr34@inbox.lv>
2026-09-16 10:41:06 +0300

    Enforce 30-day limit when editing customer bookings

--- server/api/bookings/[id].put.ts
+++ server/api/bookings/[id].put.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 import { requireUser } from '../../auth'
 import { dbQuery, DatabaseQueryError } from '../../db'
-import { generateSlots } from '../../../shared/slots'
+import { generateSlots, isWithinCustomerBookingWindow } from '../../../shared/slots'
 import { priceCentsForCategory } from '../../../shared/catalog'
 
 const schema = z.object({
@@ -16,6 +16,9 @@ export default defineEventHandler(async event => {
   if (!id) throw createError({ statusCode: 400, statusMessage: 'BOOKING_ID_REQUIRED' })
 
   const body = schema.parse(await readBody(event))
+  if (user.role !== 'admin' && !isWithinCustomerBookingWindow(body.date)) {
+    throw createError({ statusCode: 400, statusMessage: 'BOOKING_DATE_OUT_OF_RANGE' })
+  }
   if (!generateSlots(body.date).includes(body.time)) {
     throw createError({ statusCode: 400, statusMessage: 'INVALID_SLOT' })
   }
